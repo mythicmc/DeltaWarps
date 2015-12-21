1032e926734f4d95e1d52b781fca7e993caf24a0
Trace <tracebachi@yahoo.com>
2015-12-21 10:18:48 -0800

    Added missing license to top of files

--- src/main/java/com/yahoo/tracebachi/DeltaWarps/Commands/SWarpCommand.java
+++ src/main/java/com/yahoo/tracebachi/DeltaWarps/Commands/SWarpCommand.java
@@ -1,21 +1,31 @@
+/*
+ * This file is part of DeltaWarps.
+ *
+ * DeltaWarps is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DeltaWarps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DeltaWarps.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.yahoo.tracebachi.DeltaWarps.Commands;
 
-import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.FactionColl;
 import com.yahoo.tracebachi.DeltaWarps.DeltaWarpsPlugin;
 import com.yahoo.tracebachi.DeltaWarps.Prefixes;
 import com.yahoo.tracebachi.DeltaWarps.Runnables.AddServerWarpRunnable;
-import com.yahoo.tracebachi.DeltaWarps.Runnables.AddWarpRunnable;
-import com.yahoo.tracebachi.DeltaWarps.Storage.GroupLimits;
 import com.yahoo.tracebachi.DeltaWarps.Storage.Warp;
 import com.yahoo.tracebachi.DeltaWarps.Storage.WarpType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
-
-import java.util.HashMap;
 
 /**
  * Created by Trace Bachi (tracebachi@yahoo.com, BigBossZee) on 12/18/15.
